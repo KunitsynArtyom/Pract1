1f808436eb52babbf8e3389a325d66d07749f2f2
Cin Kun <artemkunitsyn@gmail.com>
2017-09-27 22:18:32 +0300

    Sort method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,22 @@ public class Main {
         }
     }
 
+    public static void sort(String[] args) {
+        String str;
+
+        for (int i = 0; i < args.length - 1; i++) {
+            for (int j = 0; j < args.length - i - 1; j++) {
+                if (args[j].compareTo(args[j + 1]) > 0) {
+                    str = args[j];
+                    args[j] = args[j + 1];
+                    args[j + 1] = str;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
+        sort(args);
         print(args);
     }
 }
