d6aa48b745512b9fbf602ce7ba61034fdb18bdb9
Cin Kun <artemkunitsyn@gmail.com>
2017-09-27 22:14:27 +0300

    Print method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,13 @@ package com.company;
 
 public class Main {
 
+    public static void print(String[] args) {
+        for (String s : args) {
+            System.out.println(s);
+        }
+    }
+
     public static void main(String[] args) {
-	
+        print(args);
     }
 }
